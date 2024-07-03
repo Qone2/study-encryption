c1923fc84b063520046e9e35b2b38ea6c64d9394
qone <qone@dayside.co.kr>
2024-07-03 17:40:39 +0900

    원본비교 추가

--- src/SHA256MDCExample.java
+++ src/SHA256MDCExample.java
@@ -5,6 +5,7 @@ public class SHA256MDCExample {
     public static void main(String[] args) {
         String message1 = "This is  a sample message";
         String message2 = "This is a sample  message";
+        String message3 = new String("This is  a sample message");
         try {
             // SHA-256 MessageDigest 객체 생성
             MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -12,10 +13,12 @@ public class SHA256MDCExample {
             // 메시지 다이제스트 생성
             byte[] digest1 = md.digest(message1.getBytes());
             byte[] digest2 = md.digest(message2.getBytes());
+            byte[] digest3 = md.digest(message3.getBytes());
 
             // 생성된 다이제스트를 16진수로 변환하여 출력
             System.out.println("MDC1: " + bytesToHex(digest1));
             System.out.println("MDC2: " + bytesToHex(digest2));
+            System.out.println("MDC3: " + bytesToHex(digest3));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
